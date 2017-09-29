539907a90afc0db56502d089a086c1daca3b15ae
Baljit Rai <bsrai91@gmail.com>
2017-09-29 19:11:29 +0000

    first commit for branch

---
+++ public/scripts/app.js
@@ -0,0 +1,26 @@
+  function createHexElement(map_name, map_d) {
+    var $list = $("<li>").addClass("hex")
+      .append($("<div>", {
+          "class": "hexIn"
+        }).append($("<a>", {
+            "class": "hexLink"
+          }).attr("href", "#").append($("<img>").attr("src", map)).append($("<h1>")
+            .text(map_name).append($("<p>").text(description, user_name)))
+
+          return $list;
+        }
+
+
+
+        router.get('', function(req, res) {
+          knex()
+        })
+
+        function select(id) {
+          knex("maps").SELECT("map_name", "map_description").WHERE("user_id", id).then((res) => {
+            for (let x in res) {
+              createHexElement(res[x].map_name);
+              createHexElement(res[x].map_description;)
+            }
+          });
+        }
