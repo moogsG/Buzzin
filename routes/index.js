bb3e79ff2f45dbd7e45a978a46ffcfbd87253b5c
Morgan Greff <morgan@greffs.com>
2017-10-02 00:15:33 +0000

    getting ready for merge

--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,11 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-
     let object = {
         user: null
     }
     res.render('index', object);
 
 });
+
+module.exports = router;
